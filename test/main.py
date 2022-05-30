9200291473800b43e65421c50dfda20e1842200c
MuNeNICK <72444129+MuNeNICK@users.noreply.github.com>
2022-05-30 12:45:33 +0900

    change

--- test/main.py
+++ test/main.py
@@ -1,4 +1,40 @@
 
-def dayStart():
+class TestWolf:
+
+    def __init__(self):
+        pass
+    def initialize(self):
+        pass
+    def dayStart(self):
+        pass
+    def firstDay(self):
+        pass
+    def finish(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    test = TestWolf()
+    test.initialize()
     
+    dayStart()
+
+    while True:
+        if firstDay() == 1:
+            print("夜から始まるよ:]")
+            print("人狼同士の囁き:[")
+            print("占い:>")
+
+        print("お昼だよー:o")
+            #お昼のフェーズ
+            for _ in range(50):
+                print("お話するよ:3")   
+                print("投票するよ")
+
+        print("夜だよー:D")
+            #夜のフェーズ
+
+
+        
 
