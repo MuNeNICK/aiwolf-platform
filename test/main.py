e2da9b77bde415c5682704afef230bd182e10503
shiroh4ck <shiro.h4ck@gmail.com>
2022-06-06 11:48:17 +0900

    Co-authored-by: MuNeNICK <MuNeNICK@users.noreply.github.com>

--- test/main.py
+++ test/main.py
@@ -1,5 +1,8 @@
+import random
+from asyncio.windows_events import NULL
 
-class TestWolf:
+
+class wolfWolf:
     
     def __init__(self):
         #ゲームのデータ管理をインスタンス変数にて行う
@@ -15,36 +18,91 @@ class TestWolf:
         self.guardvoteResult = []
         self.devineResult = 0
         self.devineJudge = 0
+        self.whisperResult = []
+        self.wolfCount = 0
+        self.humanCount = 0
+        
         
     def initialize(self):
-        #エージェントの初期化（生存 = 1, 死亡 = 0, 追放 = 2）
+        #エージェントの初期化（死亡 = 0, 生存している人間 = 1, 生存している人狼 = 2, 追放 = 3）
         for x in range(1,self.agentCount+1):
             self.agentMembers[x] = 1
-            
-        #占いの
+        
+        #エージェント1~3の誰かに人狼を割り当て
+        randomWolf = random.randint(1, self.agentCount)
+        self.agentMembers[randomWolf] = 2
+        
         
     def dayStart(self):
         #一日の開始宣言
         print(self.dayCount+"日目スタート")
+    
+    def dayEnd(self):
+        #一日の終了
+        print(self.dayCount+"一日目の終了")
+        self.dayCount += 1
         
     def finish(self):
-        #最終日の判定
-        pass
+        #ゲーム終了の判定
+        
+        #判定が1 = 人間勝利, 0 = 人狼勝利, 2 = 終了しない 
+        finishJudge = 2
+        
+        for x in self.agentMembers:
+            if x == 1:
+                self.humanCount += 1
+            elif x == 2:
+                self.wolfCount += 1
+            else:
+                continue
+        
+        if self.humanCount <= self.wolfCount:
+            finishJudge = 0
+        elif self.wolfCount == 0:
+            finishJudge = 1
+        elif self.humanCount == 0:
+            finishJudge = 0
+        
+        return finishJudge
+        
+                    
+        '''
+        finishJudge = 0
+        finishwJudge = 1
+        finishhJudge = 1
+        for x in self.agentMembers:
+            if x == 1:
+                finishhJudge = 0
+            elif x == 2:
+                finishwJudge = 0
+            else:
+                continue
+        
+        if finishhJudge == 1:
+            if finishwJudge == 0:
+                finishJudge = 1
+        
+        if finishwJudge == 1:
+            if finishhJudge == 0:
+                finishJudge = 1
+    
+        if finishJudge == 1:
+            print("")
+        '''        
+            
         
     def dayPhase(self):
-        #投票先の決定
-        
-        #エージェントを順番に呼び出す
-        #エージェントは1人一回ずつ喋る（初日の場合は役職の宣言も）
-        pass
+        print("お昼")
+    
     def nightPhase(self):
         #占い先の決定
         #人狼の釣り先決定
         #狩人の守り先決定
-        pass
+        print("夜")
     
     def inputVote(self, selfNumber):
         print("投票先一覧")
+        
         for x in self.agentMembers:
             if x == selfNumber:
                 continue
@@ -99,35 +157,45 @@ class TestWolf:
         
     def attack(self):
         print("噛み実行")
-        if self.whisperResult[dayCount] != NULL:
         
-    
+        ##噛み先
+        attackAgent = self.whisperResult[self.dayCount]
+        
+        if attackAgent != None:
+            if attackAgent != self.guardvoteResult[self.dayCount]:
+                self.agentMembers[attackAgent] = 0         
+
 
 if __name__ == "__main__":
-    test = TestWolf()
-    test.initialize()
+    wolf = wolfWolf()
+    wolf.initialize()
     
     print("夜から始まるよ:]")
     print("人狼同士の囁き:[")
     print("占い:>")
-    test.nightPhase()
+    wolf.nightPhase()
     
-    test.dayStart()
+    wolf.dayStart()
 
     while True:
-        print("お昼だよー:o")
+        #print("お昼だよー:o")
         #お昼のフェーズ
-        test.dayPhase()
-        '''
-        for _ in range(50):
-            print("お話するよ:3")   
-            print("投票するよ")
+        wolf.dayPhase()
+        
+        #終了判定
+        if wolf.finish() == 1:
+            print("ゲーム終了")
 
-        '''
+        for _ in range(wolf.dayCount):
+            wolf.inputVote()
+
         print("夜だよー:D")
         #夜のフェーズ
-        test.nightPhase()
+        wolf.nightPhase()
+    
         
-        #最終日判定
-        test.finish()
+        
+        #終了判定
+        if wolf.finish() == 1:
+            print("ゲーム終了")
 
